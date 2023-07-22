634b898c8847a6655ff3eb82f1ad9f5fec6e436d
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-22 14:02:31 +0300

    Finished OnBoarding Screen

--- Foodie/Views/OnBoarding/OnboardingCollectionViewCell.swift
+++ Foodie/Views/OnBoarding/OnboardingCollectionViewCell.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - Variables
+    
+    static let identifier = "OnboardingCell"
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var slideTitleLabel: UILabel!
+    @IBOutlet weak var slideDescriptionLabel: UILabel!
+    @IBOutlet weak var slideImageView: UIImageView!
+    
+    
+    // MARK: - Calls Methods
+    
+    func setup(_ slide: OnboardingModel) {
+        slideImageView.image = slide.image
+        slideTitleLabel.text = slide.title
+        slideDescriptionLabel.text = slide.description
+    }
 }
--- Foodie/Views/OnBoarding/OnboardingViewController.swift
+++ Foodie/Views/OnBoarding/OnboardingViewController.swift
@@ -17,19 +17,80 @@ class OnboardingViewController: UIViewController {
     
     // MARK: - Variables
     
-    var slides: [OnboardingModel] = []
+    var slides: [OnboardingModel] = [
+        OnboardingModel(title: "Delicous Dishes", description: "Expereince a variety of amazing dishes from different cultures around the world", image: UIImage(named: "slide1")!),
+        OnboardingModel(title: "World-Class Chefs", description: "Our Dishes are prepared by only the best", image: UIImage(named: "slide2")!),
+        OnboardingModel(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world", image: UIImage(named: "slide3")!)
+    ]
+    var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == slides.count - 1 {
+                nextButton.setTitle("Get Started", for: .normal)
+            }
+            else {
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
     
     // MARK: - Class Methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupCollectionView()
+        
     }
+    
     
     // MARK: - IBAction
     
     @IBAction func nextButtonClicked(_ sender: Any) {
+        if currentPage == slides.count - 1 {
+            print("Next Screen")
+        }
+        else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+        
     }
     
+}
+
+// MARK: - Extensions
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        cell.setup(slides[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    
+    func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        pageControl.isUserInteractionEnabled = false
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+    }
 }
