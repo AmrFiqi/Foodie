18cc140248b55ded228e4dbd03d64256470f2ce9
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-24 20:23:42 +0300

    added shadows to the category view cell

---
+++ Foodie/CustomViews/CardView/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Foodie
+//
+//  Created by Amr El-Fiqi on 24/07/2023.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initSetup()
+    }
+    
+    private func initSetup() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 0.1
+        // Make all card views have corner radius of 10
+        cornerRadius = 10
+    }
+}
