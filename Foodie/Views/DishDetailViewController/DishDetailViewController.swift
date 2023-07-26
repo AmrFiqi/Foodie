56876fd748f5f4a2e7b1b904d71cb7e24b42c8ef
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-26 22:24:58 +0300

    Created Dish Detail View controller on storyboard

---
+++ Foodie/Views/DishDetailViewController/DishDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DishDetailViewController.swift
+//  Foodie
+//
+//  Created by Amr El-Fiqi on 26/07/2023.
+//
+
+import UIKit
+
+class DishDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
